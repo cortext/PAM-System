4cabcec1156cd6ce170f422474b07b0f3cc36c3f
Juan Pablo O <gnu.pablo@gmail.com>
2020-11-24 14:55:28 +0100

    Produce index using the defined mapping

--- pam/schema/es_index.py
+++ pam/schema/es_index.py
@@ -1,7 +1,24 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 from elasticsearch import Elasticsearch
 from _mapping import entities
- 
+
 elastic = Elasticsearch(hosts=["elastic"])
+
+# make an API call to the Elasticsearch cluster
+# and have it return a response:
+response = elastic.indices.create(
+    index="entities",
+    body=entities,
+    ignore=400  # ignore 400 already exists code
+)
+
+if 'acknowledged' in response:
+    if response['acknowledged'] is True:
+        print("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+elif 'error' in response:
+    print("ERROR:", response['error']['root_cause'])
+    print("TYPE:", response['error']['type'])
+
+# print out the response:
+print('\nresponse:', response)
